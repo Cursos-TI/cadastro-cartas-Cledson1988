12c7cef69d43a349332c6b7445b0325a8545f9e8
Cledson1988 <cledson88.cs@gmail.com>
2026-01-27 01:24:25 +0000

    adcionando infos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    char Estado, Código da carta, Nome da Cidade;
+    char Estado, Carta, Nome da Cidade;
     int População, Número de Pontos Turísticos;
     float Área em Km², PIB;
 
+    printf("Carta: \n");
+    scanf("%d", &Carta);
+
+    printf("Estado: \n");
+    scanf("%s", &Estado);
  
 
 
